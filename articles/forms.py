dda9884f61b23fd034516d5e28cfd8ba6558b804
Natalia García Cofré <nmt.garcia.c@gmail.com>
2026-05-12 17:47:05 -0400

    front-end primera version

--- articles/forms.py
+++ articles/forms.py
@@ -4,5 +4,6 @@ from django import forms
 class ExcelUploadForm(forms.Form):
     excel_file = forms.FileField(
         label="Selecciona un archivo Excel",
-        help_text="Formatos permitidos: .xlsx"
+        help_text="Formatos permitidos: .xlsx",
+        widget=forms.ClearableFileInput(attrs={"class": "form-control"}),
     )
