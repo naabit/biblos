5fbf12be5d3175938e37684190f707ba362f2d4e
Natalia García Cofré <nmt.garcia.c@gmail.com>
2026-06-30 20:36:34 -0400

    Agrega importación normalizada para Scopus y WoS

--- articles/admin.py
+++ articles/admin.py
@@ -1,9 +1,38 @@
 from django.contrib import admin
 from .models import Article
 
+from django.contrib import admin
+
+from .models import Article, ExcelUpload
+
+
+@admin.register(ExcelUpload)
+class ExcelUploadAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "source_database",
+        "session_key",
+        "uploaded_at",
+    )
+    list_filter = ("source_database", "uploaded_at")
+    search_fields = ("name", "session_key")
+
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("title", "year", "source", "status")
-    list_filter = ("status", "year", "source")
-    search_fields = ("title", "authors", "abstract", "keywords", "doi")
+    list_display = (
+        "title",
+        "year",
+        "doi",
+        "source",
+        "external_id",
+        "status",
+    )
+    list_filter = ("status", "year")
+    search_fields = (
+        "title",
+        "authors",
+        "doi",
+        "external_id",
+        "source",
+    )
